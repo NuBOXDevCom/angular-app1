5d39b6ad5d6b65c28da1babf09a7c492fc23e9e8
NuBOX DevCom <slye@nubox.fr>
2018-03-31 22:59:03 +0200

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Post} from './post';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  posts: Post[] = [
+    {
+      title: 'Mon super titre 1',
+      content: 'Animalis messiss, tanquam lotus cobaltum.',
+      loveIts: 3,
+      created_at: new Date()
+    },
+    {
+      title: 'Mon super titre 2',
+      content: 'Everything we do is connected with art: dogma, faith, result, advice.',
+      loveIts: -2,
+      created_at: new Date()
+    },
+    {
+      title: 'Mon super titre 3',
+      content: 'Confucius says: i die and i contact. i wrestle and i preach. i disturb and i fail.',
+      loveIts: 1,
+      created_at: new Date()
+    }
+  ];
 }
